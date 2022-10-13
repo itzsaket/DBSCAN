bd73fd28f794c3d62619ee7eb4e99f3c6ae4c5e7
Saket Mishra <saket2000c@gmail.com>
2022-10-13 15:53:01 +0530

    added clustring algo

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+import math
 import os
 
 import numpy
@@ -13,6 +15,25 @@ from os import path
 from csv import reader
 from csv import reader
 
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.mixture import GaussianMixture
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib import style
+
+import psutil
+import collections
+import seaborn as sns
+import threading
+import time
+from threading import *
+
+epsilon = 0 # the radius of a neighborhood centered on a given point
+
+sns.set()
 
 # q_lat = 28.41597778
 # q_lng = 77.00401111
@@ -21,8 +42,11 @@ from csv import reader
 q_lat = 28.41641111
 q_lng = 77.00456944
 
-start_point = ["MSG"," 1049560672"," Mission: 1 Takeoff"]
-end_point = ["MSG"," 1085781277"," Mission: 6 WP"]
+start_point = ["MSG"," 1057402335"," Mission: 3 WP"]
+# end_point = ["MSG"," 1085781277"," Mission: 6 WP"]
+# start_point = ["MSG"," 1270853105"," Mission: 27 WP"]
+# end_point = ["MSG"," 1275123088"," Mission: 30 WP"]
+end_point = ["MSG"," 1275123088"," Mission: 30 WP"]
 
 # wrap an angle into 360 degrees
 def wrap360(ang):
@@ -82,8 +106,8 @@ def read_log():
             _check = False
         elif row[0] == 'R21F' and _check:
 
-            dis = row[-3]
-            ang = float(row[-2])
+            dis = row[-5]
+            ang = float(row[-4])
             ang = wrap360(att_yaw + ang)
             if float(dis) > 0.0:
                 lat2, lon2, r = geopy.distance.distance(kilometers=float(dis) / 1000).destination(
@@ -95,8 +119,8 @@ def read_log():
                 list_lat.append(r_lat)
                 list_lng.append(r_lng)
         elif row[0] == 'R21B' and _check:
-            dis = row[-3]
-            ang = float(row[-2])
+            dis = row[-5]
+            ang = float(row[-4])
             ang = wrap360(att_yaw + ang)
             if float(dis) > 0.0:
                 lat2, lon2, r = geopy.distance.distance(kilometers=float(dis) / 1000).destination(
@@ -109,12 +133,103 @@ def read_log():
                 list_lng.append(r_lng)
     dict = {'lat': list_lat, 'lon': list_lng}
     df = pd.DataFrame({key: pd.Series(value) for key, value in dict.items()})
+    if path.exists("plot_coordinate.csv"):
+        os.remove("plot_coordinate.csv")
+    df.to_csv('plot_coordinate.csv')
     dic = {'lat': list_Olat, 'lon': list_Olng}
-    df = pd.DataFrame({key: pd.Series(value) for key, value in dic.items()})
-    df.to_csv('plot_geo.csv')
-    plt.scatter(list_lat, list_lng)
-    plt.show()
+    dfo = pd.DataFrame({key: pd.Series(value) for key, value in dic.items()})
+    if path.exists("plot_geo.csv"):
+        os.remove("plot_geo.csv")
+    dfo.to_csv('plot_geo.csv')
+    # plt.scatter(list_lat, list_lng)
+    # plt.show()
+    # sample_cluster(list_lat, list_lng)
+
+x = 0.0
+y = 0.0
+
+sema = Semaphore(1)
+
+database_size = 80
+database_x = [0] * database_size
+database_y = [0] * database_size
+database_ID = [0] * database_size    # id verification
+distance_glitch = 0.002
+def get_distance(x1, y1, x2, y2):
+    return math.sqrt(pow((x2-x1), 2) + pow((y2-y1), 2))
+def point_validit(x, y, index):
+    global database_x, database_y, database_ID, database_size, distance_glitch
+    minimum_point = 0
+    for i in range(0, database_size):
+        if i == index:
+            continue
+        if database_x[i] == 0.0 and database_y[i] == 0.0:
+            continue
+        if database_ID[i] != 0:
+            continue
+        distance = get_distance(x, y, database_x[i], database_y[i])
+        if (distance < distance_glitch):
+            minimum_point += 1
+    if minimum_point > 10:
+        database_ID[index] = 1
+    else:
+        database_ID[index] = 0
+
+    # print("checking cluster")
+def filter_thread():
+    global database_x, database_y, database_ID, database_size
+    while(True):
+        # print("thread_working")
+        sema.acquire()
+        print(database_x)
+        print(database_y)
+        print(database_ID)
+        for i in range(0, database_size):
+            if database_x[i] == 0.0 and database_y[i] == 0.0:
+                continue
+            if database_ID[i] != 0:
+                print("point is good")
+                continue
+            point_validit(database_x[i], database_y[i], i)
+
+        sema.release()
+        time.sleep(0.5)
+
+
+def plotting_thread():
+    global database_x, database_y, database_ID, database_size
+    # opening the CSV file
+    file_read = open('plot_coordinate.csv',newline='')
+    csv_reader = reader(file_read)
+    counter = 0
+    count_append = 0
+    global x, y
+    for i in csv_reader:
+        if counter == 0:
+            counter += 1
+            continue
+        counter += 1
+        sema.acquire()
+        x = float(i[1])
+        y = float(i[2])
+        if count_append >= database_size:
+            count_append = 0
+
+        database_x[count_append] = x
+        database_y[count_append] = y
+        database_ID[count_append] = 0
+        count_append += 1
+
+        sema.release()
+        time.sleep(0.5)
 
 if __name__=="__main__":
+    t1 = threading.Thread(target=filter_thread, name="t1")
+    t2 = threading.Thread(target=plotting_thread, name="t2")
+    # while(True):
     init_fs()
     read_log()
+
+    t1.start()
+    t2.start()
+
